35f80309f6e43d4a018e06fc68cc33b4b498cc21
AngelNext <contact@angelnext.dev>
2023-02-12 19:09:51 +0100

    chore: Removed PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-import AstroPWA from "@vite-pwa/astro";
 import compress from "astro-compress";
 
 export default defineConfig({
 	site: "https://angelnext.dev/",
-	integrations: [
-		prefetch(),
-		sitemap({
-			customPages: [
-				"https://angelnext.dev/",
-				"https://angelnext.dev/work",
-				"https://angelnext.dev/work/loxt",
-				"https://angelnext.dev/work/pyrite",
-			],
-		}),
-		AstroPWA({
-			registerType: "autoUpdate",
-			manifestFilename: "app.webmanifest",
-			manifest: {
-				name: "AngelNext",
-				short_name: "AngelNext",
-				description: "The personal site of AngelNext.",
-				display: "standalone",
-				background_color: "#ffffff",
-				theme_color: "#7611a6",
-				icons: [
-					{
-						src: "/angelnext-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "angelnext-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-					},
-					{
-						src: "angelnext-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-				],
-			},
-			devOptions: {
-				enabled: true,
-			},
-		}),
-		compress(),
-	],
+	integrations: [prefetch(), sitemap(), compress()],
 });
